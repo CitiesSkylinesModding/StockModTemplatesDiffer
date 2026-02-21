3e6ff51dc4d12d62c4edbc165472cdc8dd9a5803
Morgan Touverey Quilling <morgan@mtq.io>
2026-02-21 18:28:36 +0100

    chore: update templates for version 1.5.4f1

--- ui/types/ui.d.ts
+++ ui/types/ui.d.ts
@@ -530,6 +530,7 @@ declare module "cs2/ui" {
   export interface InfoRowProps extends ClassProps {
   	icon?: string;
   	left?: ReactNode;
+  	center?: ReactNode;
   	right?: ReactNode;
   	tooltip?: ReactNode;
   	link?: ReactNode;
@@ -540,7 +541,7 @@ declare module "cs2/ui" {
   	noShrinkRight?: boolean;
   	justifyLeft?: boolean;
   }
-  export const InfoRow: ({ icon, left, right, tooltip, link, uppercase, subRow, subRowDimmed, disableFocus, className, noShrinkRight, justifyLeft }: InfoRowProps) => JSX.Element;
+  export const InfoRow: ({ icon, left, center, right, tooltip, link, uppercase, subRow, subRowDimmed, disableFocus, className, noShrinkRight, justifyLeft }: InfoRowProps) => JSX.Element;
   export interface SimplePanelProps extends PanelProps {
   	draggable?: false | undefined;
   }
@@ -592,7 +593,7 @@ declare module "cs2/ui" {
   export interface ScrollableProps {
   	horizontal?: boolean;
   	vertical?: boolean;
-  	trackVisibility?: "always" | "scrollable";
+  	trackVisibility?: "always" | "scrollable" | "reserve";
   	overshootX?: number;
   	overshootY?: number;
   	smooth?: boolean;
@@ -604,6 +605,7 @@ declare module "cs2/ui" {
   	onOverflowY?: (overflow: boolean) => void;
   	autoScroll?: boolean;
   	autoScrollSettings?: AutoScrollSettings;
+  	useNewStyle?: boolean;
   }
   export export const Scrollable: (props: ScrollableProps & {
   	children?: import("react").ReactNode;
